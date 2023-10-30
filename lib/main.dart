15e45fbdbfc8631f57c7ba0e7d3c15089fe340ec
TRosa Dev <thiagosrosa@gmail.com>
2023-10-29 22:38:57 -0300

    refactor: split GradientContainer into another file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'widgets/gradient_container.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,35 +13,6 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: GradientContainer(),
-      ),
-    );
-  }
-}
-
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
-
-  @override
-  Widget build(context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
       ),
     );
   }
