2eb4d02b87c6802602f27fccaa40ea5f3cf26588
TRosa Dev <thiagosrosa@gmail.com>
2023-11-02 16:37:42 -0300

    refactor: changed var to const

--- lib/widgets/gradient_container/index.dart
+++ lib/widgets/gradient_container/index.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'widgets/styled_text/index.dart';
 
-var beginAlignment = Alignment.topLeft;
-var endAlignment = Alignment.bottomRight;
+const beginAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -10,9 +10,9 @@ class GradientContainer extends StatelessWidget {
   @override
   Widget build(context) {
     return Container(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         gradient: LinearGradient(
-          colors: const [
+          colors: [
             Color.fromARGB(255, 26, 2, 80),
             Color.fromARGB(255, 45, 7, 98),
           ],
